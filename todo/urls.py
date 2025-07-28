0c5cce616f42a16d0e0fdbc2abf0cdb9a1019e5a
deepakkumawat <dk286322@gmail.com>
2025-07-28 11:16:40 +0530

    todo app final

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,14 @@ from.import views
 urlpatterns = [
 
     path('addTask/',views.addTask,name='addTask'),
+    # marks as done 
+    path('mark_as_done/<int:pk>/',views.mark_as_done,name='mark_as_done'),
+    # marks as under 
+    path('mark_as_under/<int:pk>/',views.mark_as_under,name='mark_as_under'),
+    #edit feature
 
+    path('edit/<int:pk>/',views.edit,name='edit'),
+    
+    path('delete/<int:pk>/',views.delete,name='delete'),
   
-]
+] 
